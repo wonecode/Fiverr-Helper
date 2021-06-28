7997dbad53979122e185bafa7a4c9da59a71f505
RedPandore <tennessee-houry_student2021@wilder.school>
2021-06-28 15:20:40 +0200

    Make Entity and fixture for Quest

---
+++ src/Repository/QuestRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Quest;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Quest|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Quest|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Quest[]    findAll()
+ * @method Quest[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class QuestRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Quest::class);
+    }
+
+    // /**
+    //  * @return Quest[] Returns an array of Quest objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('q')
+            ->andWhere('q.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('q.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Quest
+    {
+        return $this->createQueryBuilder('q')
+            ->andWhere('q.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
