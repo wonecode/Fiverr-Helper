c22683d28fc5b68bab0e451d51ad2e957687dd53
glochlainn <lochlainn.gadault@gmail.com>
2021-06-28 22:25:39 +0200

    header home and whats fiver section

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,7 @@ Encore
     .addEntry('app', './assets/app.js')
 
     .addStyleEntry('navbar', './assets/styles/navbar.scss')
+    .addStyleEntry('home', './assets/styles/home.scss')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
