c3083b17494b03f6009544c0d53cb6012c6b7395
RedPandore <tennessee-houry_student2021@wilder.school>
2021-06-29 13:59:20 +0200

    Experience management

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,19 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Service\ExperienceCalculator;
 
 class AdminController extends AbstractController
 {
-    private ExperienceCalculator $experience;
     
     /**
      * @Route("/admin", name="admin")
      */
-    public function index(ExperienceCalculator $experience): Response
+    public function index(): Response
     {
-        dd($experience->test()); 
-
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
