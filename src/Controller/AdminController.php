d40761e052c5a21b3e88dfc19ea1405323c1656c
RedPandore <tennessee-houry_student2021@wilder.school>
2021-06-29 11:53:57 +0200

    Gest

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,14 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\ExperienceCalculator;
 
 class AdminController extends AbstractController
 {
+    private ExperienceCalculator $experience;
+    
     /**
      * @Route("/admin", name="admin")
      */
-    public function index(): Response
+    public function index(ExperienceCalculator $experience): Response
     {
+        dd($experience->test()); 
+
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
