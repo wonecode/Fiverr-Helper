fef891842e0d67b79388ec1ea82f61c2d8a6b0fe
bouboumael <bouboumael@gmail.com>
2021-06-29 03:46:27 +0200

    save

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Help;
 use App\Entity\Comment;
 use App\Form\CommentType;
 use App\Repository\CommentRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/comment", name="comment_")
