773c185354c5a44576f65ac38dc02e2349dd4d8b
bouboumael <bouboumael@gmail.com>
2021-06-29 16:03:54 +0200

    save

---
+++ src/Controller/RankingController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RankingController extends AbstractController
+{
+    /**
+     * @Route("/ranking", name="ranking")
+     */
+    public function index(): Response
+    {
+        return $this->render('ranking/index.html.twig', [
+            'controller_name' => 'RankingController',
+        ]);
+    }
+}
