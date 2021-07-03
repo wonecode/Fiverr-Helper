b3ba19f8999dcc43fafeb850d0589564d675e3ac
bouboumael <mael-chariault_student2021@wilder.school>
2021-07-03 20:29:35 +0200

    params

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-use App\Service\ExperienceCalculator;
-use App\Service\UserGeneratorApi;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,7 +11,7 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(ExperienceCalculator $xp): Response
+    public function index(): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
